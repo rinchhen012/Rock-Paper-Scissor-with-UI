4277e5f2a387ab667f61501948245f65c3d48ce8
rinchhen012 <rinchhen.meh@gmail.com>
2020-02-16 23:24:50 +0900

    Final UI commit

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 
+let userScore = 0,
+    botScore = 0,
+    result,
+    bot,
+    user,
+    tracker;
+
 function computerPlay() {
     // function to generate computers(bot) choice 
     const random = Math.random() * 3;
@@ -6,31 +13,73 @@ function computerPlay() {
 }
 
 function playRound() {
-    let bot = computerPlay().toLowerCase();
-
-    let user = prompt('Type any one (Rock/Paper/Scissor)').toLowerCase().trim();
+    bot = computerPlay().toLowerCase();
+    user = (this.className).toLowerCase();
 
     if (user === bot)
-        return 'Draw';
+        result = 'draw';
 
     // Rock scenarios
     if (user === 'rock' && bot === 'paper')
-        return 'You Lose';
+        result = 'lose';
     if (user === 'rock' && bot === 'scissor')
-        return 'You Win';
+        result = 'win';
 
     // Paper scenarios
     if (user === 'paper' && bot === 'scissor')
-        return 'You Lose';
+        result = 'lose';
     if (user === 'paper' && bot === 'rock')
-        return 'You Win';
+        result = 'win';
 
     // Scissor scenarios
     if (user === 'scissor' && bot === 'rock')
-        return 'You Lose';
+        result = 'lose';
     if (user === 'scissor' && bot === 'paper')
-        return 'You Win';
+        result = 'win';
+
+    roundReferee();
 }
 
+function reset() {
+    let choice = prompt('Are you sure ? Y / N ');
+    if (choice.toLowerCase() === 'y') {
+        userScore = 0;
+        botScore = 0;
+        p1.textContent = `User: ${userScore}`;
+        p2.textContent = `Bot: ${botScore}`;
+        p3.textContent = '';
+    }
+    else if (choice.toLowerCase() === 'n') return;
+    else alert('Wrong input try again!');
+}
 
+function roundReferee() {
+    if (result === 'win') {
+        userScore += 1;
+        tracker = 'You Win!!!';
+    }
+    else {
+        userScore += 1;
+        botScore += 1;
+        tracker = 'Draw'
+    }
 
+    p1.textContent = `You: ${userScore}`;
+    p2.textContent = `Bot: ${botScore}`;
+    p3.textContent = `${tracker}`;
+}
+
+let resetGame = document.querySelector('#reset-button');
+resetGame.addEventListener('click', reset);
+
+let selection = Array.from(document.querySelectorAll('#button'));
+selection.forEach(button => button.addEventListener('click', playRound));
+
+let div = document.querySelector('#result');
+let p1 = document.createElement('p');
+let p2 = document.createElement('p');
+let p3 = document.createElement('p');
+
+div.appendChild(p1);
+div.appendChild(p2);
+div.appendChild(p3);
